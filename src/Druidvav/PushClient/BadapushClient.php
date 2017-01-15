0210aafa7d52b7aeccf8f82190418a0703259d77
Антон Власов <druidvav@gmail.com>
2017-01-15 18:39:58 +0300

    Badapush service support

---
+++ src/Druidvav/PushClient/BadapushClient.php
@@ -0,0 +1,58 @@
+<?php
+namespace Druidvav\PushClient;
+
+use Druidvav\PushClient\Entity\Payload;
+use Druidvav\PushClient\Exception\ClientException;
+use Druidvav\PushClient\Exception\InternalErrorException;
+use Druidvav\PushClient\Exception\InvalidSubscribeIdException;
+
+class BadapushClient
+{
+    protected $apiUrl = 'http://badapush.ru/api/v1/jsonrpc';
+    protected $apiKey;
+
+    public function __construct($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    public function sendPayload(Payload $payload)
+    {
+        $ch = curl_init($this->apiUrl);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+            'id' => 1,
+            'method' => 'sendPayload',
+            'params' => [
+                'device_id' => $payload->getDeviceId(),
+                'payload' => $payload->getPayload()
+            ]
+        ]));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'X-Authorization-Token: ' . $this->apiKey,
+            'Content-Type: application/json',
+        ]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $errno = curl_errno($ch);
+        $error = curl_error($ch);
+        curl_close($ch);
+
+        $data = json_decode($response, true);
+        if (!empty($data['result']['result'])) {
+            if ($data['result']['result'] == 'ok') {
+                return $data['result']['response'];
+            } elseif ($data['result']['result'] == 'error') {
+                switch ($data['result']['error_code']) {
+                    default: throw new ClientException($data['result']['error_message']);
+                    case 'error': throw new ClientException($data['result']['error_message']);
+                    case 'invalid_id': throw new InvalidSubscribeIdException($data['result']['error_message']);
+                    case 'internal_error': throw new InternalErrorException($data['result']['error_message']);
+                }
+            }
+        } elseif (!empty($data['error'])) {
+            throw new ClientException($data['error']['code'] . ': ' . $data['error']['message']);
+        }
+        throw new ClientException($httpcode . '/' . $errno . ': ' . ($error ?: $response));
+    }
+}
